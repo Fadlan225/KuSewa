8cbed1baac0a559c01357ada004d820fe13f3185
Fadlan079 <fadlanfirdaus220@gmail.com>
2026-08-20 18:18:54 +0800

    feat: initialize Laravel Echo with Reverb configuration

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -6,10 +6,12 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: window.location.hostname,
-    wsPort: 8081,
-    wssPort: 8081,
-    forceTLS: false,
-    disableStats: true,
-    enabledTransports: ['ws'],
+
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: 80,
+    wssPort: 443,
+
+    forceTLS: true,
+
+    enabledTransports: ['ws', 'wss'],
 });
