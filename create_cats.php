520c08bdb4fd7602e4bde969736a4fc1fde710b2
Fadlan225 <fadlanfirdaus225@gmail.com>
2026-09-18 15:27:14 +0800

    chore: remove temporary development scripts and update package-lock.json

--- create_cats.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$cats = ['Bangunan Kos', 'Fasilitas Bersama', 'Bangunan Kos Dari Jalan', 'Depan Kamar'];
-foreach ($cats as $cat) {
-    App\Models\galery_category::firstOrCreate(['name' => $cat]);
-}
-echo "Done\n";
